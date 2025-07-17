daa32805c8b82881389bf38889feabee4e17b423
Dobstar2 <128965154+Dobstar2@users.noreply.github.com>
2025-07-17 13:19:38 +0100

    Add service worker for offline support

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'park-companion-v3';
+const OFFLINE_URLS = [
+  './',
+  './index.html',
+  './magic-kingdom.html',
+  './epcot.html',
+  './hollywood-studios.html',
+  './animal-kingdom.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET' || !event.request.url.startsWith(self.location.origin)) {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
